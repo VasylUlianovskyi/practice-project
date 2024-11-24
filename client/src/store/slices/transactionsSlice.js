f11e72dcb090a54afdb15469e8f0b7fe70b93053
vasyl <vasia.kit434@gmail.com>
2024-11-24 15:44:53 +0200

    Add date-fns, rrefactor transactionsSlice

--- client/src/store/slices/transactionsSlice.js
+++ client/src/store/slices/transactionsSlice.js
@@ -1,5 +1,6 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import * as restController from '../../api/rest/restController';
+import { decorateAsyncThunk } from '../../utils/store';
 
 const TRANSACTIONS_SLICE_NAME = 'transactions';
 
@@ -10,23 +11,50 @@ const initialState = {
 };
 
 // /transactions/get
-export const getTransactionsThunk = createAsyncThunk(
-  `${TRANSACTIONS_SLICE_NAME}/get`,
-  async (payload, { rejectWithValue }) => {
-    try {
-      const transactions = await restController.getTransactions();
-      console.log(transactions);
-      return transactions;
-    } catch (err) {
-      return rejectWithValue(err);
-    }
-  }
-);
+// export const getTransactionsThunk = createAsyncThunk(
+//   `${TRANSACTIONS_SLICE_NAME}/get`,
+//   async (payload, { rejectWithValue }) => {
+//     try {
+//       const { data } = await restController.getTransactions();
+//       return data;
+//     } catch (err) {
+//       return rejectWithValue(err);
+//     }
+//   }
+// );
+
+export const getTransactionsThunk = decorateAsyncThunk({
+  key: `${TRANSACTIONS_SLICE_NAME}/get`,
+  thunk: async payload => {
+    const { data } = await restController.getTransactions();
+    return data;
+  },
+});
+
+const reducers = {};
+
+const extraReducers = builder => {
+  builder.addCase(getTransactionsThunk.pending, state => {
+    state.isFetching = true;
+    state.error = null;
+  });
+  builder.addCase(getTransactionsThunk.fulfilled, (state, { payload }) => {
+    state.isFetching = false;
+    state.transactions = [...payload];
+  });
+  builder.addCase(getTransactionsThunk.rejected, (state, { payload }) => {
+    state.isFetching = false;
+    state.error = payload;
+  });
+};
+
 const transactionsSlice = createSlice({
   name: TRANSACTIONS_SLICE_NAME,
   initialState,
-  reducers: {},
+  reducers,
+  extraReducers,
 });
 
 const { reducer, actions } = transactionsSlice;
+
 export default reducer;
